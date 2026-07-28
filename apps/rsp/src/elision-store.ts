993b926a42bd4d4d2a5bdc4c22436e427b563581
Filipe Forattini <filipeforattini1@gmail.com>
2026-07-28 11:15:43 -0300

    feat(rsp): export provisionElisionStore and contentHandle
    Refs #2688

--- apps/rsp/src/elision-store.ts
+++ apps/rsp/src/elision-store.ts
@@ -1,4 +1,5 @@
 export { DEFAULT_RSP_BYTE_BUDGET, DEFAULT_RSP_EPHEMERAL_TTL_HOURS, DEFAULT_RSP_TTL_DAYS, RSP_ELISION_COLLECTION } from "./elision-store/public.js";
 export type { RspElisionRecord, RspElisionStoreOptions, RspExpiredHandle, RspLossLevel, RspLossMeta, RspMintMeta, RspRecoveryHandle, RspStorageClass, RspStorageClassStats, RspStoreStats } from "./elision-store/public.js";
 export { ensureReddbBinaryFromWarmCache, storageClassForCommand } from "./elision-store/helpers.js";
-export { RspElisionStore } from "./elision-store/store.js";
+export { contentHandle } from "./elision-store/helpers.js";
+export { provisionElisionStore, RspElisionStore } from "./elision-store/store.js";
