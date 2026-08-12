5c29cafc53dad109615058d1dd4f5aaa4c7f2573
filipeforattini <filipeforattini1@gmail.com>
2026-08-12 08:42:16 -0300

    refactor(red-castle): keep bootstrap CLI below ratchet Refs #3733

--- packages/red-castle/src/cli.ts
+++ packages/red-castle/src/cli.ts
@@ -1,14 +1,13 @@
 import { Command, Options } from "@effect/cli";
 import { FileSystem } from "@effect/platform";
-import { planGithubWrite } from "@reddb-io/github";
-import { resolveRepoSlugForDir } from "@reddb-io/shared/project-identity-resolve.js";
 import { Effect, Option } from "effect";
 import * as clack from "@clack/prompts";
-import { execFileSync, execSync } from "node:child_process";
+import { execSync } from "node:child_process";
 import { join } from "node:path";
 import { styleText } from "node:util";
 
 import { Display } from "./Display.js";
+import { createSandcastleLabel } from "./GithubBootstrap.js";
 import {
   buildImage,
   checkDockerHealth,
@@ -469,24 +468,7 @@ const initCommand = Command.make(
 
         if (shouldCreateLabel) {
           yield* Effect.try({
-            try: () => {
-              const repo = resolveRepoSlugForDir(cwd);
-              const plan = planGithubWrite([
-                "gh",
-                "label",
-                "create",
-                "Sandcastle",
-                "--description",
-                "Issues for Sandcastle to work on",
-                "--color",
-                "F9A825",
-                ...(repo ? ["--repo", repo] : []),
-              ]);
-              execFileSync(plan.args[0]!, plan.args.slice(1), {
-                cwd,
-                stdio: "ignore",
-              });
-            },
+            try: () => createSandcastleLabel(cwd),
             catch: () => undefined,
           }).pipe(Effect.ignore);
         }
